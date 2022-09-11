5786636410b859d990b65260f58d740c0522f4c3
mxdx <Capitainecosmos@protonmail.com>
2022-09-10 22:16:35 -0400

    improve: update formatting & linting dependencies, typescript & ts-node

--- src/controllers/baseController.ts
+++ src/controllers/baseController.ts
@@ -1,14 +1,15 @@
-import * as express from "express";
-import { configs } from "../../config/configs";
+import * as express from 'express';
+import { configs } from '../../config/configs';
 
 export class BaseController {
   public async getAll(
     req: express.Request,
     res: express.Response,
-    next: express.NextFunction
+    next: express.NextFunction,
   ): Promise<void> {
     req.log.info(`${new Date()}`);
-    res.status(200).send({ a: "hello", api: configs.api });
+
+    res.status(200).send({ a: 'hello', api: configs.api });
   }
 }
 
