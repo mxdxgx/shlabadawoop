34ab5fd00c9dea6ca3bd6a28dc9b293a45ae7314
mxdx <Capitainecosmos@protonmail.com>
2023-10-19 22:30:37 -0400

    fix: config files in order at startup

--- config/configs.ts
+++ config/configs.ts
@@ -1,6 +1,8 @@
 import * as config from 'config';
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 import { IAuthToken } from '../src/models/authToken.model';
+import * as path from 'path';
+import { PrintUtils } from '../src/utils/printUtils';
 
 export class Configuration {
   private readonly apiVersion: string;
@@ -23,6 +25,8 @@ export class Configuration {
         jwksUri: <string>config.get('auth.wellKnown.jwksUri'),
       },
     };
+
+    this.printConfigFilesInOrder();
   }
 
   get api(): unknown {
@@ -33,6 +37,25 @@ export class Configuration {
 
   get typeOrm(): PostgresConnectionOptions {
     return this.ormConfig;
+  }
+
+  /**
+   * @function printConfigFilesInOrder
+   * @private
+   * @return void
+   *
+   * Basic print function that formats the config filenames as they appear in loading order.
+   */
+  private printConfigFilesInOrder() {
+    console.log('Configuration files in order : ');
+    console.log('####################################################');
+
+    for (const source of config.util.getConfigSources()) {
+      const boundary = PrintUtils.createBoundary(path.basename(source.name));
+      console.log(boundary);
+    }
+
+    console.log('####################################################');
   }
 }
 
